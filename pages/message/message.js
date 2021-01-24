6aac7453ba586910b7aac914453f54deddadbe32
chijey <jack.wu@things-matrix.com>
2021-01-24 18:18:11 +0800

    addmessage

--- pages/message/message.js
+++ pages/message/message.js
@@ -1,18 +1,54 @@
-// pages/message/message.js
+// pages/exchange/exchange.js
+import request from '../../utils/request.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    currentData : 0,
+    interactionList: [{
+      nickName: '',
+      message: '',
+      doctorOpenid: '',
+      userOpenid: '',
+      date: '',
+      avatarUrl: ''
+    }]
+  },
 
+  //获取当前滑块的index
+  bindchange:function(e){
+    const that  = this;
+    that.setData({
+      currentData: e.detail.current
+    })
+  },
+  stopTouchMove: function() {
+    return false;
+  },
+  //点击切换，滑块index赋值
+  checkCurrent:function(e){
+    const that = this;
+    if (that.data.currentData === e.target.dataset.current){
+        return false;
+    }else{
+      that.setData({
+        currentData: e.target.dataset.current
+      })
+    }
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    let userInfo = wx.getStorage({
+      key: 'userInfo',
+    })
+    let opendId = userInfo.opendId;
 
+    
   },
 
   /**
@@ -26,9 +62,34 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    let i = 0;
+    let userInfo = wx.getStorageSync("userInfo");
+    let openId = userInfo.openId;
+    let authnizatin =  wx.getStorageSync("Authorization");
+    request.get('/api/chat/'+openId, {}, {Authorization:authnizatin}).then(res => {
+      console.log(res);
+      res.data.data.forEach((item) => {
+        console.log(item.openId)
+        this.setData({
+          ['interactionList['+i+']'] : {
+            doctorOpenid: item.openId,
+            userOpenid: item.openId,
+            doctorName: item.nickName,
+            message: item.openId,
+            date: item.openId,
+            avatarUrl: item.avatarUrl
+          }
+        })
+        i++;
+      })
+    })
   },
-
+  userButtonTap(e) {
+    console.log('user button tap', e)
+    wx.navigateTo({
+      url: '/pages/consultation/consultation?thisUserOpenid=' + e.currentTarget.dataset.data.userOpenid+'&otherUserOpenid=' + e.currentTarget.dataset.data.doctorOpenid+'&doctorName='+e.currentTarget.dataset.data.doctorName,
+    })
+  },
   /**
    * 生命周期函数--监听页面隐藏
    */
---
+++ utils/request.js
@@ -0,0 +1,41 @@
+var request = {};
+request.options = {
+  baseUrl: 'http://localhost:8000'
+}
+
+request.get = function (url, data, header = {}) {
+  return new Promise((resovle, reject) => {
+    wx.request({
+      url: request.options.baseUrl + url,
+      dataType: 'json',
+      header: Object.assign({'Cookie': 'JSESSIONID='+wx.getStorageSync('sessionid')}, {'Cookie': 'JSESSIONID='+wx.getStorageSync('sessionid')}, header),
+      data: data,
+      success: function(res) {
+        resovle(res);
+      },
+      fail: function (err) {
+        reject(err);
+      }
+    })
+  })
+}
+
+request.post = function (url, data) {
+  return new Promise((resovle, reject) => {
+    wx.request({
+      url: request.options.baseUrl + url,
+      dataType: 'json',
+      header: Object.assign({'Cookie': 'JSESSIONID='+wx.getStorageSync('sessionid')}, {'Cookie': 'JSESSIONID='+wx.getStorageSync('sessionid')}),
+      data: data,
+      method:"POST",
+      success: function(res) {
+        resovle(res);
+      },
+      fail: function (err) {
+        reject(err);
+      }
+    })
+  })
+}
+
+module.exports  = request;
